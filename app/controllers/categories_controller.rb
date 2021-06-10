3ac23d9706179eead16be64fb12221e30d66faf5
jithesh <jithesh@poshvine.com>
2021-06-10 17:41:45 +0530

    categories changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 	before_action :require_admin, except: [:index, :show]
-	
+
 	def show
 		@category = Category.find(params[:id])
 	end	
@@ -12,6 +12,20 @@ class CategoriesController < ApplicationController
 			redirect_to @category
 		else
 			render 'new'
+		end
+	end
+
+	def edit
+		@category = Category.find(params[:id])
+	end
+
+	def update
+	   @category = Category.find(params[:id])
+		if @category.update(category_params)
+			flash[:notice] = "Successfully updated the category"
+			redirect_to category_path
+		else
+			render 'update'
 		end
 	end
 
